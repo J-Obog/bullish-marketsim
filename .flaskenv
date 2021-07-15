81442993b65fe4d0821d9bd1544b389e2aede4b9
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-07-14 21:17:43 -0400

    bootstrapped simple flask server

---
+++ .flaskenv
@@ -0,0 +1,4 @@
+FLASK_APP=marketsim
+FLASK_ENV=development
+FLASK_DEBUG=True
+FLASK_RUN_PORT=8089
--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-#setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='marketsim',
+    version='1.0',
+    description='Stock market simulation',
+    url='https://github.com/J-Obog/market-simulator',
+    author='JObog',
+    author_email='jobogbaimhe@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests'])
+)
