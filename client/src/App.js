37d57703473c5801e613de0037cefad0acc9895b
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-08-25 19:07:51 -0400

    added auth context api

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { PrivateRoute, PublicRoute } from "./router-hooks"; 
-import { Dashboard, Login, Logout, Orders, Portfolio, Signup, Stock, NoMatch } from "./pages"; 
+import React, { useState } from "react";
+
+//routing
+import { Switch, BrowserRouter as Router } from "react-router-dom";
+import PrivateRoute from "./routing/PrivateRoute";
+import PublicRoute from "./routing/PublicRoute";
+
+//pages
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout"; 
+import Orders from "./pages/Orders";
+import Portfolio from "./pages/Portfolio";
+import Signup from "./pages/Signup";
+import Stock from "./pages/Stock"; 
+import NoMatch from "./pages/NoMatch"; 
+
+import Navbar from './components/core/Navbar';
+import AuthContext from "./context/AuthContext"; 
 
 const App = () => {
+    const [token, setToken] = useState("");
+
     return (
         <Router>
-            <Switch>  
-                <PublicRoute path="/login" component={Login} exact restricted/>
-                <PublicRoute path="/signup" component={Signup} exact restricted/>
-                <PrivateRoute path="/" component={Dashboard} exact />
-                <PrivateRoute path="/orders" component={Orders} exact />
-                <PrivateRoute path="/portfolio" component={Portfolio} exact />
-                <PrivateRoute path="/stock/:id" component={Stock} exact />
-                <PrivateRoute path="*" component={NoMatch} exact />
-            </Switch>
+            <AuthContext.Provider value={{ token, setToken }}>
+              <Navbar/>
+              <Switch>  
+                  <PublicRoute path="/login" component={Login} exact restricted/>
+                  <PublicRoute path="/signup" component={Signup} exact restricted/>
+                  <PrivateRoute path="/" component={Dashboard} exact />
+                  <PrivateRoute path="/orders" component={Orders} exact />
+                  <PrivateRoute path="/portfolio" component={Portfolio} exact />
+                  <PrivateRoute path="/stock/:id" component={Stock} exact />
+                  <PrivateRoute path="*" component={NoMatch} exact />
+              </Switch>
+            </AuthContext.Provider>
         </Router>
     );
 }
