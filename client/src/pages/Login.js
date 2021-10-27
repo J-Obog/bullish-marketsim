6bb50ae93d24777d329fd75296b8063029e52b8d
JObog <jobogbaimhe@gmail.com>
2021-10-27 08:07:19 -0400

    simplified nav structure

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'; 
+import React, { useState, useContext } from 'react'; 
 import { Link } from 'react-router-dom';
 import axios from "axios"; 
+import { AuthContext } from '../context/AuthContext';
 
 const Login = () => {
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState(""); 
     const [erros, setErrors] = useState(null);
+    const { login } = useContext(AuthContext); 
 
     const changeEmail = (e) => {
         setEmail(e.target.value); 
@@ -18,19 +20,19 @@ const Login = () => {
     const attemptLogin = () => {
         axios.post("http://localhost:4001/api/auth/login", { email: email, password: password})
         .then(({data}) => {
-
+            login(data.access_token, data.refresh_token); 
         })
-        .catch(err => {
-            console.error(err);
+        .catch(({response}) => {
+            const {data} = response; 
+            console.error(data);
         })
     }
 
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className="bg-white w-1/4 px-16 py-8 rounded-md shadow-xl flex flex-col items-center justify-center">
+            <div className="bg-white w-1/4 px-16 py-8 rounded-xl md shadow-xl flex flex-col items-center justify-center">
                 <div className="mb-16">
-                    <h1>Bullish Market Sim</h1>
-                    <h2>Some other text</h2>
+                    <h1>Login</h1>
                 </div>
                 <div className="mb-12">
                     <div className="mb-8">
@@ -38,7 +40,7 @@ const Login = () => {
                     </div>
                     <div>
                         <input onInput={changePassword} size="30" type="password" placeHolder="Password" 
-                        className={`(1) ? "" : "outline-none border-b-2 border-gray-200 focus:border-green-300`}/>
+                        className="outline-none border-b-2 border-gray-200 focus:border-green-300"/>
                     </div>
                 </div>
                 <div className="text-sm mb-10">
