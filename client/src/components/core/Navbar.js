9584ea4d4131555f96d04a55e653ef2d3d0a0de0
JObog <jobogbaimhe@gmail.com>
2021-10-28 21:46:45 -0400

    Revert "added search bar to nav"
    This reverts commit 9507c98e18bf6e5a8ba2a881073955606c3a9746.

--- client/src/components/core/Navbar.js
+++ client/src/components/core/Navbar.js
@@ -5,17 +5,9 @@ const Navbar = () => {
     return (
             <div className="bg-white py-4 px-4 flex flex-row items-center justify-between">
                 <div className="flex flex-row">
-                    <div className="mr-8 flex flex-row border border-gray-200 rounded-md px-2 py-0.5 hover:shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#CBD5E1">
-                            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                        </svg>
-                        <input placeHolder="Lookup a Stock" className="bg-transparent outline-none"/>
-                    </div>
-                    <div>
-                        <Link to="/" className="text-gray-800 mr-4 hover:text-green-300">Trading</Link>
-                        <Link to="/orders" className="text-gray-800 mr-4 hover:text-green-300">Orders</Link>
-                        <Link to="/portfolio" className="text-gray-800 mr-4 hover:text-green-300">Portfolio</Link>
-                    </div>
+                    <Link to="/" className="text-gray-800 mr-4 hover:text-green-300">Trading</Link>
+                    <Link to="/orders" className="text-gray-800 mr-4 hover:text-green-300">Orders</Link>
+                    <Link to="/portfolio" className="text-gray-800 mr-4 hover:text-green-300">Portfolio</Link>
                 </div>
                 <div className="flex flex-row">
                     <Link to="#" className="text-gray-800 mr-4 hover:text-green-300">Account</Link>
